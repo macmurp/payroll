2e83a34c075b896e66442f4df3b765182c0718e0
macmurp <macmurp@gmail.com>
2020-06-11 16:01:14 -0700

    salaried employee wip

--- Program.cs
+++ Program.cs
@@ -9,42 +9,52 @@ namespace payroll
             
         }
     }
-    abstract class Employee
+    public abstract class Employee
     {
-        string Firstname { get; set; }
-        string Lastname { get; set; }
-        int Age { get; set; }
-        int EmployeeID { get; set; }
-        Employee(string Firstname, string Lastname, int Age, int EmployeeID)
+        public abstract string Firstname { get; set; }
+        public abstract string Lastname { get; set; }
+        public abstract int Age { get; set; }
+        public abstract int EmployeeID { get; set; }
+        public Employee(string Firstname, string Lastname, int Age, int EmployeeID)
         {
             this.Firstname = Firstname;
             this.Lastname = Lastname;
             this.Age = Age;
             this.EmployeeID = EmployeeID;
         }
-        string Nameoutput()
+        public string Nameoutput()
         {
             string Nameoutput = Lastname + ", " + Firstname;
             return Nameoutput;
         }
-        string Ageoutput()
+        public string Ageoutput()
         {
             string Ageoutput = Age + " years old";
             return Ageoutput;
 
         }
-        string IDoutput()
+        public string IDoutput()
         {
             string IDoutput = "ID " + EmployeeID;
             return IDoutput;
         }
     }
-    class HourlyEmployee : Employee
+    public class SalariedEmployee : Employee
     {
-        string Firstname { get; set; }
-        string Lastname { get; set; }
-        int Age { get; set; }
-        int EmployeeID { get; set; }
-        int MonthlyPay { get; set; }
+        public decimal Monthlypay { get; set; }
+        public override string Firstname { get; set; }
+        public override string Lastname { get; set; }
+        public override int Age { get; set; }
+        public override int EmployeeID { get; set; }
+
+        public SalariedEmployee(string Firstname, string Lastname, int Age, int EmployeeID, decimal Monthlypay)
+            : base(Firstname, Lastname, Age, EmployeeID)
+        {
+            this.Firstname = Firstname;
+            this.Lastname = Lastname;
+            this.Age = Age;
+            this.EmployeeID = EmployeeID;
+            this.Monthlypay = Monthlypay;
+        }
     }
 }
