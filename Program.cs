b5e6ab42232732ff450b73f2ca6eda191786d4f5
macmurp <macmurp@gmail.com>
2020-06-12 19:17:25 -0700

    string tests and unit tests

--- Program.cs
+++ Program.cs
@@ -15,12 +15,13 @@ namespace payroll
                 Console.WriteLine("1.Add a salaried employee");
                 Console.WriteLine("2.Add an hourly employee");
                 Console.WriteLine("3.Generate report");
-                //input = UserInput(Console.ReadLine());
                 input = UserInput.IntInput(Console.ReadLine());
+                //old error checker before i used the UserInput class left below
                 //if(!int.TryParse(Console.ReadLine(), out input))
                 //{ Console.WriteLine("Please enter a valid option."); continue; }
                 switch (input)
                 {
+                    //switch case menu options
                     case 1:
                         string tempfirst;
                         string templast;
@@ -29,22 +30,22 @@ namespace payroll
                         decimal tempsalary;
                         Console.WriteLine("Enter the following details:");
                         Console.WriteLine("First name");
-                        tempfirst = Console.ReadLine();
+                        //tempfirst = Console.ReadLine();
+                        tempfirst = UserInput.StringInput(Console.ReadLine());
+                        if (tempfirst is null) continue;
                         Console.WriteLine("Last name");
-                        templast = Console.ReadLine();
+                        templast = UserInput.StringInput(Console.ReadLine());
+                        if (templast is null) continue;
                         Console.WriteLine("Age");
-                        if(!int.TryParse(Console.ReadLine(), out tempage))
-                        { Console.WriteLine("Please enter a valid option."); continue; }
+                        tempage = UserInput.IntInput(Console.ReadLine());
+                        if (tempage == 0) continue;
+                        //when the IntInput checker returns a 0 it means the input was not an int, so this skips it
                         Console.WriteLine("Employee ID");
-                        if (!int.TryParse(Console.ReadLine(), out tempid))
-                        { Console.WriteLine("Please enter a valid option."); continue; }
+                        tempid = UserInput.IntInput(Console.ReadLine());
+                        if (tempid == 0) continue;
                         Console.WriteLine("Monthly salary");
-                        //if (!decimal.TryParse(Console.ReadLine(), out tempsalary))
-                        //{ Console.WriteLine("Please enter a valid option."); continue; }
                         tempsalary = UserInput.DecInput(Console.ReadLine());
-                        if (tempsalary == 0)
-                        { continue; }
-                            
+                        if (tempsalary == 0) continue;         
                         Employees.Add(new SalariedEmployee(tempfirst, templast, tempage, tempid, tempsalary));
                         break;
                     case 2:
@@ -52,21 +53,23 @@ namespace payroll
                         decimal temphours;
                         Console.WriteLine("Enter the following details:");
                         Console.WriteLine("First name");
-                        tempfirst = Console.ReadLine();
+                        tempfirst = UserInput.StringInput(Console.ReadLine());
+                        if (tempfirst is null) continue;
                         Console.WriteLine("Last name");
-                        templast = Console.ReadLine();
+                        templast = UserInput.StringInput(Console.ReadLine());
+                        if (templast is null) continue;
                         Console.WriteLine("Age");
-                        if (!int.TryParse(Console.ReadLine(), out tempage))
-                        { Console.WriteLine("Please enter a valid option."); continue; }
+                        tempage = UserInput.IntInput(Console.ReadLine());
+                        if (tempage == 0) continue;
                         Console.WriteLine("Employee ID");
-                        if (!int.TryParse(Console.ReadLine(), out tempid))
-                        { Console.WriteLine("Please enter a valid option."); continue; }
+                        tempid = UserInput.IntInput(Console.ReadLine());
+                        if (tempid == 0) continue;
                         Console.WriteLine("Hourly pay");
-                        if (!decimal.TryParse(Console.ReadLine(), out temphourly))
-                        { Console.WriteLine("Please enter a valid option."); continue; }
+                        temphourly = UserInput.DecInput(Console.ReadLine());
+                        if (temphourly == 0) continue;     
                         Console.WriteLine("Hours worked");
-                        if (!decimal.TryParse(Console.ReadLine(), out temphours))
-                        { Console.WriteLine("Please enter a valid option."); continue; }
+                        temphours = UserInput.DecInput(Console.ReadLine());
+                        if (temphours == 0) continue;
                         Employees.Add(new HourlyEmployee(tempfirst, templast, tempage, tempid, temphourly, temphours));
                         break;
                     case 3:
@@ -75,6 +78,7 @@ namespace payroll
                         decimal hourlytotal = 0M;
                         foreach (Employee s in Employees)
                         {
+                            //employee output loop for report
                             Console.WriteLine(s.NameOutput() + " | " + s.IDOutput() + " | " + s.AgeOutput() + " | " + s.CalculatePay() + "\n");
                             total += s.CalculatedPay;
                             if(s is SalariedEmployee)
@@ -85,6 +89,7 @@ namespace payroll
                                 hourlytotal += s.CalculatedPay;
                             }
                         }
+                        //rounding, addition, and tax outputs here
                         Console.Write("Total gross payout $" + total);
                         Console.Write(" | Net paid: $" + decimal.Round(total - (total * .265M) - (total * .0765M), 2));
                         Console.Write(" | FICA paid: $" + decimal.Round((total * .0765M), 2));
@@ -108,6 +113,8 @@ namespace payroll
     }
     public static class UserInput
     {
+        //used to have small checks after inputs in main, but couldn't unit test them there
+        //moving them here allowed me to call them in main and in my unit test
         static int input;
         static decimal decimalinput;
         public static int IntInput(string i)
@@ -115,6 +122,7 @@ namespace payroll
             if (!int.TryParse(i, out input))
             {
                 Console.WriteLine("Please enter a valid option."); return 0;
+                //returns a 0 if it cannot parse
             }
             else
                 return input;
@@ -128,6 +136,18 @@ namespace payroll
             else
                 return decimalinput;
         }
+        public static string StringInput(string i)
+        {
+            if (decimal.TryParse(i, out decimal temp))
+            {
+                //if string entered is convertable to a decimal, then it likely isn't a name so this rejects it
+                Console.WriteLine("Please enter a valid option."); return null;
+            }
+            else
+            {
+                return i;
+            }
+        }
     }
     public abstract class Employee
     {
@@ -136,8 +156,10 @@ namespace payroll
         private int Age { get; set; }
         private int EmployeeID { get; set; }
         public decimal CalculatedPay { get; set; }
+        //pay is declared within employee at first, defined in the subclasses later
         protected Employee(string Firstname, string Lastname, int Age, int EmployeeID)
         {
+            //constructor
             this.Firstname = Firstname;
             this.Lastname = Lastname;
             this.Age = Age;
@@ -145,6 +167,7 @@ namespace payroll
         }
         public string NameOutput()
         {
+            //outputs to help when final report is used
             return "Name: " + Lastname + ", " + Firstname;
         }
         public string AgeOutput()
@@ -167,6 +190,7 @@ namespace payroll
         public SalariedEmployee(string Firstname, string Lastname, int Age, int EmployeeID, decimal Monthlypay)
             : base(Firstname, Lastname, Age, EmployeeID)
         {
+            //additional monthly pay constructor arg
             this.Monthlypay = Monthlypay;
             CalculatedPay = Monthlypay;
         }
@@ -179,6 +203,7 @@ namespace payroll
         public HourlyEmployee(string Firstname, string Lastname, int Age, int EmployeeID, decimal Hourlypay, decimal Hoursworked)
             : base(Firstname, Lastname, Age, EmployeeID)
         {
+            //additional hours and hourly pay constructor args
             this.Hourlypay = Hourlypay;
             this.Hoursworked = Hoursworked;
             CalculatedPay = Hoursworked * Hourlypay;
